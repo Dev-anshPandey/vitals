d4ac296c2d240367bb277f5872fe7a75f585b6a4
Devansh <devanshpandey87@gmail.com>
2022-11-12 12:46:38 +0530

    Added stepcounter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vitals/dahboard.dart';
 import 'package:vitals/screen/home_screen.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       // theme: ThemeData(
       //   primarySwatch: Colors.blue,
       // ),
-      home:  HomeScreen(),
+      home:  dashboard(),
     );
   }
 }
