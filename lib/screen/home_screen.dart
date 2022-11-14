48ffa73a32440110558df5f607ef1127a8d2a933
Devansh <devanshpandey87@gmail.com>
2022-11-15 00:14:07 +0530

    added piechart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,23 +1,18 @@
 import 'dart:ui';
 
-import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
-import 'package:vitals/widget/DashBoardCard.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:sensors_plus/sensors_plus.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:math';
+import 'dart:async';
 
-double x = 0.0;
-double y = 0.0;
-double z = 0.0;
-double addValue = 0.025;
-int steps = 0;
-double previousDistacne = 0.0;
-double distance = 0.0;
+import 'package:pedometer/pedometer.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:pie_chart/pie_chart.dart';
+import 'package:vitals/widget/color.dart';
+
+String formatDate(DateTime d) {
+  return d.toString().substring(0, 19);
+}
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -25,78 +20,256 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late Stream<StepCount> _stepCountStream;
+  late Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = '?', _steps = '0';
+  double a = 0.0;
+  Map<String, double> dataMap = {};
+  @override
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Map<String, double> pieSteps(double steps) {
+    print(steps);
+    return dataMap = {
+      "Steps": steps,
+      "": 2000 - steps,
+    };
+  }
+
+  void onStepCount(StepCount event) {
+    print(event);
+    setState(() {
+      pieSteps(event.steps.toDouble());
+      a = event.steps.toDouble();
+      _steps = event.steps.toString();
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    print(event);
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    print('onPedestrianStatusError: $error');
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+    print(_status);
+  }
+
+  void onStepCountError(error) {
+    print('onStepCountError: $error');
+    setState(() {
+      _steps = 'Step Count not available';
+    });
+  }
+
+  void initPlatformState() async {
+    if (await Permission.activityRecognition.request().isGranted) {
+      _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+      _pedestrianStatusStream
+          .listen(onPedestrianStatusChanged)
+          .onError(onPedestrianStatusError);
+
+      _stepCountStream = Pedometer.stepCountStream;
+      _stepCountStream.listen(onStepCount).onError(onStepCountError);
+    }
+    if (!mounted) return;
+  }
+
   @override
   Widget build(BuildContext context) {
     String date = DateFormat.MMMMEEEEd().format(DateTime.now());
     return Scaffold(
-      backgroundColor: Color(0xffF3F2E9),
-      body: StreamBuilder<AccelerometerEvent>(
-          stream: SensorsPlatform.instance.accelerometerEvents,
-          builder: (context, snapShort) {
-            if (snapShort.hasData) {
-              x = snapShort.data!.x;
-              y = snapShort.data!.y;
-              z = snapShort.data!.z;
-              distance = getValue(x, y, z);
-              if (distance > 20) {
-                steps++;
-              }
-            }
-            return SafeArea(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: MediaQuery.of(context).size.height * 0.01,
-                        top: MediaQuery.of(context).size.height * 0.01),
-                    child: Text(
-                      date,
-                      style: TextStyle(
-                          backgroundColor: Color(0xffF3F2E9),
-                          fontSize: MediaQuery.of(context).size.height * 0.017,
-                          letterSpacing: 0.4),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: MediaQuery.of(context).size.height * 0.01,
-                        top: MediaQuery.of(context).size.height * 0.02),
-                    child: Text(
-                      "Summary",
-                      style: TextStyle(
-                        backgroundColor: Color(0xffF3F2E9),
-                        fontSize: MediaQuery.of(context).size.height * 0.032,
-                        letterSpacing: 0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  dashboardCard(steps)
-                ],
+        backgroundColor: mycolor['bgColor'],
+        bottomNavigationBar: bottomNavigation(),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.height * 0.01,
+                    top: MediaQuery.of(context).size.height * 0.01),
+                child: Text(
+                  date,
+                  style: TextStyle(
+                      backgroundColor: mycolor['bgColor'],
+                      fontSize: MediaQuery.of(context).size.height * 0.019,
+                      letterSpacing: 0.4),
+                ),
               ),
-            );
-          }),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.height * 0.01,
+                    top: MediaQuery.of(context).size.height * 0.02),
+                child: Text(
+                  "Summary",
+                  style: TextStyle(
+                    backgroundColor: mycolor['bgColor'],
+                    fontSize: MediaQuery.of(context).size.height * 0.032,
+                    letterSpacing: 0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.02,
+                    right: MediaQuery.of(context).size.width * 0.025,
+                    left: MediaQuery.of(context).size.width * 0.025),
+                child: Card(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "Steps :",
+                            style: TextStyle(
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.02),
+                          ),
+                          Text(_steps,
+                              style: TextStyle(
+                                  fontSize: MediaQuery.of(context).size.height *
+                                      0.035,
+                                  fontWeight: FontWeight.bold))
+                        ],
+                      ),
+                      // SizedBox(
+                      //   width: 20,
+                      // ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.02,
+                            bottom: MediaQuery.of(context).size.height * 0.02),
+                        child: PieChart(
+                          dataMap: pieSteps(a),
+                          chartRadius: MediaQuery.of(context).size.width / 2.5,
+                          chartType: ChartType.ring,
+                          legendOptions:
+                              const LegendOptions(showLegends: false),
+                          chartValuesOptions: const ChartValuesOptions(
+                            showChartValuesInPercentage: true,
+                            showChartValuesOutside: true,
+                          ),
+                          colorList: [Color(0xffE7FE55), Colors.black],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.height * 0.01,
+                    top: MediaQuery.of(context).size.height * 0.02),
+                child: Text(
+                  "Trends",
+                  style: TextStyle(
+                    backgroundColor: mycolor['bgColor'],
+                    fontSize: MediaQuery.of(context).size.height * 0.022,
+                    letterSpacing: 0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Trend(bgColor:mycolor['gradientColor']),
+                  Trend(bgColor: mycolor['textColor'],)
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class Trend extends StatelessWidget {
+  Color? bgColor;
+  Trend({this.bgColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:  EdgeInsets.only(left:MediaQuery.of(context).size.width*0.02,right:MediaQuery.of(context).size.width*0.07,top: MediaQuery.of(context).size.height*0.02),
+      child: Container(
+        color: bgColor,
+        height: 200,
+        width: 160,
+      ),
     );
   }
+}
 
-  double getValue(double x, double y, double z) {
-    double magnitude = sqrt(x * x + y * y + z * z);
-    getPreviousValue();
-    double modDistance = magnitude - previousDistacne;
-    setPreviousValue(magnitude);
-    return modDistance;
-  }
+class bottomNavigation extends StatefulWidget {
+  int index = 0;
+  @override
+  State<bottomNavigation> createState() => _bottomNavigationState();
+}
 
-  void setPreviousValue(double distance) async {
-    SharedPreferences _pref = await SharedPreferences.getInstance();
-    _pref.setDouble("preValue", distance);
-  }
-
-  void getPreviousValue() async {
-    SharedPreferences _pref = await SharedPreferences.getInstance();
-    setState(() {
-      previousDistacne = _pref.getDouble("preValue") ?? 0.0;
-    });
+class _bottomNavigationState extends State<bottomNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        backgroundColor: mycolor['bgColor'],
+        onTap: (state) {
+          setState(() {
+            widget.index = state;
+          });
+        },
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
+        currentIndex: widget.index,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.black,
+              size: 30,
+            ),
+            activeIcon: CircleAvatar(
+              child:
+                  Icon(Icons.home, color: mycolor['gradientColor'], size: 30),
+              backgroundColor: mycolor['textColor'],
+              maxRadius: 25,
+            ),
+            label: "",
+            backgroundColor: Color(0xff8BDF85),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.group, color: Colors.black, size: 30),
+              activeIcon: CircleAvatar(
+                child: Icon(Icons.group,
+                    color: mycolor['gradientColor'], size: 30),
+                backgroundColor: mycolor['textColor'],
+                maxRadius: 25,
+              ),
+              label: "",
+              backgroundColor: Color(0xff8BDF85)),
+          // BottomNavigationBarItem(
+          //     icon: Icon(
+          //       Icons.account_circle,
+          //       color: Colors.black,
+          //       size: 35,
+          //     ),
+          //     activeIcon: CircleAvatar(
+          //       child:
+          //           Icon(Icons.account_circle, color: mycolor['gradientColor'], size: 35),
+          //       backgroundColor: mycolor['textColor'],
+          //       maxRadius: 25,
+          //     ),
+          //     label: "",
+          //     backgroundColor: Color(0xff8BDF85)),
+        ]);
   }
 }
---
+++ lib/widget/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+Map<String,Color> mycolor = {
+  "bgColor":Color(0xffF3F2E9),
+   'gradientColor' :Color(0xffE7FE55),
+   "textColor" : Colors.black,
+   "thirdColor" : Colors.white
+};
